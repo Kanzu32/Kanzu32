6afc7e971041112dcaf09c52c5770db7122ea0df
Kanzu32 <kanzu32@gmail.com>
2019-09-08 12:06:23 +0300

    apple and some fixes

--- snakeGame/js/game.js
+++ snakeGame/js/game.js
@@ -16,6 +16,18 @@ let food = {
 	y: Math.floor(Math.random() * 15 + 3) * box,
 }
 
+function spawnFood() {
+	food = {
+		x: Math.floor(Math.random() * 17 + 1) * box,
+		y: Math.floor(Math.random() * 15 + 3) * box,
+	}
+	for (let i = 0; i < snake; i++) {
+		if (food.x == snake[i].x && food.y == snake[i].y) {
+			spawnFood();
+		}
+	}
+}
+
 let snake = [];
 snake[0] = {
 	x: 9 * box,
@@ -49,6 +61,9 @@ function drawGame() {
 	ctx.drawImage(ground, 0, 0);
 	ctx.drawImage(foodImg, food.x, food.y);
 
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
 	for (let i = 0; i < snake.length; i++) {
 		ctx.fillStyle = i == 0 ? "DarkGreen" : "ForestGreen";
 		ctx.fillRect(snake[i].x, snake[i].y, box, box);
@@ -58,23 +73,18 @@ function drawGame() {
 	ctx.font = "50px Arial";
 	ctx.fillText(score, box * 2.5, box * 1.7);
 
-	let snakeX = snake[0].x;
-	let snakeY = snake[0].y;
+	
 
 	if (snakeX == food.x && snakeY == food.y) {
 		score++;
-
-		food = {
-			x: Math.floor(Math.random() * 17 + 1) * box,
-			y: Math.floor(Math.random() * 15 + 3) * box,
-		}
+		spawnFood();
+		// food = {
+		// 	x: Math.floor(Math.random() * 17 + 1) * box,
+		// 	y: Math.floor(Math.random() * 15 + 3) * box,
+		// }
 	} else {
 		snake.pop();
 	}
-
-	if (snakeX < box || snakeX > box * 17
-		|| snakeY < box *  3 || snakeY > box * 17)
-		clearInterval(game);
 
 	if (dir == "left") snakeX -= box;
 	if (dir == "right") snakeX += box;
@@ -87,6 +97,10 @@ function drawGame() {
 	}
 
 	eatTail(newHead, snake);
+
+	if (snakeX < box || snakeX > box * 17
+	 	|| snakeY < box *  3 || snakeY > box * 17)
+	 	clearInterval(game);
 
 	snake.unshift(newHead);
 }
