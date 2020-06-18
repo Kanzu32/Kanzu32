37b37519621a4e79ad976467ef2ec3c0221c637c
Kanzu <kanzu32@gmail.com>
2020-06-18 14:49:13 +0300

    Add old projects

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let projects = [
 	{name: "Room Text Game", link: "roomGame/index.html", img: "img/roomGame.png"},
 	{name: "ToDo", link: "todo/index.html", img: "img/todo.png"},
 	{name: "cssDraw#1", link: "cssDraw1/index.html", img: "img/cssDraw1.png"},
+	{name: "Breakout", link: "Breakout/index.html", img: "img/Breakout.png"},
+	{name: "Slider", link: "Slider/index.html", img: "img/Slider.png"},
+	{name: "SiteEx#1", link: "SiteEx1/index.html", img: "img/SiteEx1.png"},
+	{name: "SiteEx#2", link: "SiteEx2/index.html", img: "img/SiteEx2.png"},
 ];
 
 window.onload = function() {
