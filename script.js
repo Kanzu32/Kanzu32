3758f73cc41c33495182627dba25e271e36f4db7
Kanzu <kanzu32@gmail.com>
2021-07-20 18:43:55 +0300

    New Design
    Finaly... )
    This needs some changes lаter.

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-let main;
-
 let projects = [
-	{name: "Snake Game", link: "snakeGame/index.html", img: "img/snakeGame.png"},
-	{name: "Timer", link: "TIMER/index.html", img: "img/timer.png"},
-	{name: "Room Text Game", link: "roomGame/index.html", img: "img/roomGame.png"},
-	{name: "ToDo", link: "todo/index.html", img: "img/todo.png"},
-	{name: "cssDraw#1", link: "cssDraw1/index.html", img: "img/cssDraw1.png"},
-	{name: "Breakout", link: "Breakout/index.html", img: "img/Breakout.png"},
-	{name: "Slider", link: "Slider/index.html", img: "img/Slider.png"},
-	{name: "SiteEx#1", link: "SiteEx1/index.html", img: "img/SiteEx1.png"},
-	{name: "SiteEx#2", link: "SiteEx2/index.html", img: "img/SiteEx2.png"},
-	{name: "Matrix(not work on mobile)", link: "matrix/index.html", img: "img/matrix.png"},
-	{name: "HypnoSphere", link: "HypnoSphere/index.html", img: "img/hypnosphere.png"},
-	{name: "fox.", link: "fox/index.html", img: "img/fox.png"},
-	{name: "Spaceship", link: "spaceship/index.html", img: "img/spaceship.png"},
-	{name: "Rock Scissors Paper", link: "rps/index.html", img: "img/rps.png"},
+  {type: "img", name: "Snake Game", link: "snakeGame/index.html", img: "img/snakeGame.png"},
+	{type: "img", name: "Timer", link: "TIMER/index.html", img: "img/timer.png"},
+	{type: "img", name: "Room Text Game", link: "roomGame/index.html", img: "img/roomGame.png"},
+	{type: "img", name: "ToDo", link: "todo/index.html", img: "img/todo.png"},
+	{type: "img", name: "cssDraw#1", link: "cssDraw1/index.html", img: "img/cssDraw1.png"},
+	{type: "img", name: "Breakout", link: "Breakout/index.html", img: "img/Breakout.png"},
+	{type: "img", name: "Slider", link: "Slider/index.html", img: "img/Slider.png"},
+	{type: "img", name: "SiteEx#1", link: "SiteEx1/index.html", img: "img/SiteEx1.png"},
+	{type: "img", name: "SiteEx#2", link: "SiteEx2/index.html", img: "img/SiteEx2.png"},
+	{type: "img", name: "Matrix(not work on mobile)", link: "matrix/index.html", img: "img/matrix.png"},
+	{type: "img", name: "HypnoSphere", link: "HypnoSphere/index.html", img: "img/hypnosphere.png"},
+	{type: "text", name: "fox.", link: "fox/index.html", description: "Just a fox."},
+	{type: "img", name: "Spaceship", link: "spaceship/index.html", img: "img/spaceship.png"},
+	{type: "img", name: "Rock Scissors Paper", link: "rps/index.html", img: "img/rps.png"},
 ];
 
-window.onload = function() {
 
-	main = document.getElementsByTagName("main")[0];
-
-	for (let item of projects) {
-		let link = main.appendChild( document.createElement('a') );
-		link.href = `${item.link}`;
-
-		let div = link.appendChild( document.createElement('div') );
-		div.className = 'item';
-		div.style.backgroundImage = `url(${item.img})`;
-
-		let divAfter = div.appendChild( document.createElement('div') );
-		divAfter.className = 'name';
-		divAfter.textContent = item.name;
-
-	};
-	console.log(document.getElementsByTagName("main"));
-
-};
+window.onload = () => {
+  let projectsBlock = document.getElementsByClassName("projects-block")[0];
+  for (let i = 0; i < projects.length; i++)
+  {
+    if (projects[i].type == "img")
+    {
+      projectsBlock.innerHTML += `<a class="border-wrapper" href="${projects[i].link}"><div><div class="project-name">${projects[i].name.toLowerCase()}</div><div class="project-img" style="background-image: url('${projects[i].img}')"></div></div></a>`;
+    } else
+    {
+      projectsBlock.innerHTML += `<a class="border-wrapper" href="${projects[i].link}"><div><div class="project-name">${projects[i].name.toLowerCase()}</div><div class="project-description">${projects[i].description.toLowerCase()}</div></div></a>`;
+    };
+  };
+}
